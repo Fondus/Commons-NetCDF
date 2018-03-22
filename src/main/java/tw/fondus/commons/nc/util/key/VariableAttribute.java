6be7e9f1a5eee084ac56f9e8fa552ba181ff61c9
Bread God <vipcube@gmail.com>
2018-03-22 15:27:22 +0800

    Update VariableAttribute with WGS84 and TWD97 attribute.
    Signed-off-by: Bread God <vipcube@gmail.com>

--- src/main/java/tw/fondus/commons/nc/util/key/VariableAttribute.java
+++ src/main/java/tw/fondus/commons/nc/util/key/VariableAttribute.java
@@ -16,11 +16,18 @@ public class VariableAttribute {
 	
 	public static final float MISSINGVALUE = -9999;
 	public static final double MISSINGVALUE_COORDINATES = 9.96921E36;
-	public static final String COORDINATES_Y_84 = "latitude";
-	public static final String COORDINATES_X_84 = "longitude";
+	public static final String COORDINATES_Y = "projection_y_coordinate";
+	public static final String COORDINATES_X = "projection_x_coordinate";
+	public static final String COORDINATES_Y_WGS84 = "latitude";
+	public static final String COORDINATES_X_WGS84 = "longitude";
+	public static final String NAME_Y_WGS84 = "y coordinate according to WGS 1984";
+	public static final String NAME_X_WGS84 = "x coordinate according to WGS 1984";
+	public static final String NAME_Y_TWD97 = "y coordinate according to TWD 1997";
+	public static final String NAME_X_TWD97 = "x coordinate according to TWD 1997";
 	public static final String UNITS_TIME = "minutes since 1970-01-01 00:00:00.0 +0000";
-	public static final String UNITS_Y_84 = "degrees_north";
-	public static final String UNITS_X_84 = "degrees_east";
+	public static final String UNITS_Y_WGS84 = "degrees_north";
+	public static final String UNITS_X_WGS84 = "degrees_east";
+	public static final String UNITS_TWD97 = "m";
 	public static final String AXIS_X = "X";
 	public static final String AXIS_Y = "Y";
 	public static final String AXIS_Z = "Z";
